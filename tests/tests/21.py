cf2a7b73c897f070d51ba3d6830aa0e663a7d24d
SEUNGHYUN AN <seunghyun@snares-02.cs.wisc.edu>
2025-04-13 13:10:29 -0500

    test 21 fix

--- tests/tests/21.py
+++ tests/tests/21.py
@@ -15,7 +15,7 @@ def check_line(line: str):
 def check():
     f = open('metrics.log')
     cnt = [0,0,0,0,0]
-    ans = [32, 0, 64, 2]
+    ans = [32, 0, 64, 2, 0]
 
     for line in f:
         r, t = check_line(line)
